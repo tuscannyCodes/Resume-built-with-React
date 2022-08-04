6d494f7fce278fdfeca652fda39839a56178b5ca
TuscannyCodes <tuscannypolk@gmail.com>
2022-08-04 13:21:10 -0400

    minor updates

--- src/data/english.js
+++ src/data/english.js
@@ -30,7 +30,7 @@ const data = {
   summary: {
     title: 'Summary',
     text: `
-      Hello! I am a Front-end developer that has 2 years of experience designing and building websites. I am constantly expanding my knowledge and technical resume by contributing to Open Source Projects,creating web designs in Figma, and studying various coding languages at any chance can I get. I enjoy using Python, Javascript and libraries like Framer Motion to make applications and websites look and feel great.`,
+      Hello! I am a Front-end developer that has 2 years of experience designing and building websites. I enjoy expanding my knowledge and technical resume by contributing to Open Source Projects,creating web designs in Figma, and studying various coding languages at any chance can I get. I enjoy using Python, Javascript and libraries like Framer Motion, to make applications and websites look and feel great.`,
   },
 
   /**
